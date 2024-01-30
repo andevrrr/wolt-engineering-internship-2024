afd0b1cfc8cbd786a84ebe6a6ed6468500f68696
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-30 23:51:33 +0200

    src/components/main: changed useState structure+created common handleChange func

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 import Input from "./input";
 
 const DeliveryFeeCalculator: React.FC = () => {
-  const [cartValue, setCartValue] = useState<number>(0);
+  const [formState, setFormState] = useState({
+    cartValue: 0,
+    deliveryDistance: 0,
+  });
   const [error, setError] = useState<boolean>(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: Number(value) || 0,
+    }));
   };
 
   return (
@@ -16,12 +27,25 @@ const DeliveryFeeCalculator: React.FC = () => {
         <div>
           <Input
             type="number"
-            label="cartValue"
-            value={cartValue}
+            name="cartValue"
+            label="Cart Value"
+            value={formState.cartValue}
             placeholder="Please enter your cart value"
             error={error}
-            onChange={(e) => setCartValue(e.target.value)}
+            onChange={handleChange}
             dataTestId="cartValue"
+          />
+        </div>
+        <div>
+          <Input
+            type="number"
+            name="deliveryDistance"
+            label="Delivery Distance"
+            value={formState.deliveryDistance}
+            placeholder="Please enter the distance"
+            error={error}
+            onChange={handleChange}
+            dataTestId="deliveryDistance"
           />
         </div>
       </form>
