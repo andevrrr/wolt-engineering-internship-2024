4488abb90288fb6f9d12ba1e976c8932c3eec679
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-30 23:47:35 +0200

    src/components/main: added Input component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,11 +2,29 @@ import React, { useState } from "react";
 import Input from "./input";
 
 const DeliveryFeeCalculator: React.FC = () => {
-  const [cartValue, setCartValue] = useState<number>();
+  const [cartValue, setCartValue] = useState<number>(0);
+  const [error, setError] = useState<boolean>(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
       <h1>Delivery Fee Calculator</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Input
+            type="number"
+            label="cartValue"
+            value={cartValue}
+            placeholder="Please enter your cart value"
+            error={error}
+            onChange={(e) => setCartValue(e.target.value)}
+            dataTestId="cartValue"
+          />
+        </div>
+      </form>
     </div>
   );
 };
