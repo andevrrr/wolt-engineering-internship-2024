71b070bbcf091ec17ed7c8594096b13f91ebec3e
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-30 22:37:27 +0200

    src/components/main: added useState and import Input

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
+import Input from "./input";
 
 const DeliveryFeeCalculator: React.FC = () => {
+  const [cartValue, setCartValue] = useState<number>();
 
   return (
     <div>
       <h1>Delivery Fee Calculator</h1>
-
     </div>
   );
 };
